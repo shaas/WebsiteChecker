208cf151c596f48a3ea89d47f78ac57f2c1c28c9
Stefan Haas <haas273@gmail.com>
2020-11-04 19:11:44 +0100

    Fixed missing format-statements
    Signed-off-by: Stefan Haas <haas273@gmail.com>

--- Database/postgre.py
+++ Database/postgre.py
@@ -1,4 +1,7 @@
 import psycopg2
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class Database:
@@ -34,34 +37,41 @@ class Database:
             wsentries (str): Name of the table which stores all meassurements
                 of WebsiteChecker
         """
-        if not self.con:
-            self.con = psycopg2.connect(database=self.dbname,
-                                        user=self.dbuser, host=self.dbhost,
-                                        port=self.dbport, password=self.dbpass)
-            self.cursor = self.con.cursor()
-        if not self.wstable:
-            self.cursor.execute(f"SELECT to_regclass('{wstable}')")
-            self.con.commit()
-            if not self.cursor.fetchone()[0]:
-                self.cursor.execute(f"CREATE TABLE {wstable} "
-                                    "(RepHash CHAR(64)  PRIMARY KEY, "
-                                    "Url VARCHAR(255) NOT NULL);")
+        try:
+            if not self.con:
+                self.con = psycopg2.connect(database=self.dbname,
+                                            user=self.dbuser, host=self.dbhost,
+                                            port=self.dbport, password=self.dbpass)
+                self.cursor = self.con.cursor()
+
+            if not self.wstable:
+                self.cursor.execute(f"SELECT to_regclass('{wstable}')")
                 self.con.commit()
-            self.wstable = wstable
-        if not self.wsentries:
-            self.cursor.execute(f"SELECT to_regclass('{wsentries}')")
-            self.con.commit()
-            if not self.cursor.fetchone()[0]:
-                self.cursor.execute(f"CREATE TABLE {wsentries} "
-                                    "(RepHash CHAR(64) REFERENCES "
-                                    "{self.wstable} ON DELETE CASCADE, "
-                                    "Date TIMESTAMP NOT NULL, "
-                                    "Status SMALLINT NOT NULL, "
-                                    "ResponseTime FLOAT NOT NULL, "
-                                    "RegexSet BOOLEAN, RegexFound BOOLEAN, "
-                                    "PRIMARY KEY (RepHash, date));")
+                if not self.cursor.fetchone()[0]:
+                    self.cursor.execute(f"CREATE TABLE {wstable} "
+                                        "(RepHash CHAR(64)  PRIMARY KEY, "
+                                        "Url VARCHAR(255) NOT NULL);")
+                    self.con.commit()
+                self.wstable = wstable
+
+            if not self.wsentries:
+                self.cursor.execute(f"SELECT to_regclass('{wsentries}')")
                 self.con.commit()
-            self.wsentries = wsentries
+
+                if not self.cursor.fetchone()[0]:
+                    self.cursor.execute(f"CREATE TABLE {wsentries} "
+                                        "(RepHash CHAR(64) REFERENCES "
+                                        f"{self.wstable} ON DELETE CASCADE, "
+                                        "Date TIMESTAMP NOT NULL, "
+                                        "Status SMALLINT NOT NULL, "
+                                        "ResponseTime FLOAT NOT NULL, "
+                                        "RegexSet BOOLEAN, RegexFound BOOLEAN, "
+                                        "PRIMARY KEY (RepHash, date));")
+                    self.con.commit()
+                self.wsentries = wsentries
+        except psycopg2.Error as e:
+            logger.error("Unable to open the Database: %s", str(e))
+            raise Exception(e)
 
     def add_entry(self, rep_hash, url, date, status, response_time,
                   regex_set=False, regex_found=False):
@@ -76,34 +86,46 @@ class Database:
             regex_set (bool): Bool if website was checked of a regex
             regex_found (bool): Bool if regex was found on website
         """
-        self.cursor.execute(f"SELECT * FROM {self.wstable} "
-                            "WHERE RepHash = '{rep_hash}'")
-        self.con.commit()
-        if not self.cursor.fetchone():
-            self.cursor.execute(f"INSERT INTO {self.wstable} (RepHash, Url) "
-                                "VALUES ('{rep_hash}', '{url}')")
-        self.cursor.execute(f"INSERT INTO {self.wsentries} (RepHash, Date, "
-                            "Status, ResponseTime, RegexSet, RegexFound) "
-                            "VALUES ('{rep_hash}', '{date}', {status}, "
-                            "{response_time}, {regex_set}, {regex_found})")
-        self.con.commit()
+        try:
+            self.cursor.execute(f"SELECT * FROM {self.wstable} "
+                                f"WHERE RepHash = '{rep_hash}'")
+            self.con.commit()
+            if not self.cursor.fetchone():
+                self.cursor.execute(f"INSERT INTO {self.wstable} (RepHash, Url) "
+                                    "VALUES ('{rep_hash}', '{url}')")
+            self.cursor.execute(f"INSERT INTO {self.wsentries} (RepHash, Date, "
+                                "Status, ResponseTime, RegexSet, RegexFound) "
+                                f"VALUES ('{rep_hash}', '{date}', {status}, "
+                                f"{response_time}, {regex_set}, {regex_found})")
+            self.con.commit()
+        except psycopg2.Error as e:
+            logger.error("Unable to add an entry: %s", str(e))
+            raise Exception(e)
 
     def close_database(self):
         """ Closes the database connection.
         """
-        if self.con:
-            self.con.close()
-            self.con = None
-            self.cursor = None
-            self.wstable = None
+        try:
+            if self.con:
+                self.con.close()
+                self.con = None
+                self.cursor = None
+                self.wstable = None
+        except psycopg2.Error as e:
+            logger.error("Unable to close database: %s", str(e))
+            raise Exception(e)
 
     def delete_tables(self):
         """ Deletes all the tables created for WebsiteChecker.
         """
-        if self.wsentries:
-            self.cursor.execute(f"DROP TABLE IF EXISTS {self.wsentries}")
-            self.wsentries = None
-        if self.wstable:
-            self.cursor.execute(f"DROP TABLE IF EXISTS {self.wstable} CASCADE")
-            self.wstable = None
-        self.con.commit()
+        try:
+            if self.wsentries:
+                self.cursor.execute(f"DROP TABLE IF EXISTS {self.wsentries}")
+                self.wsentries = None
+            if self.wstable:
+                self.cursor.execute(f"DROP TABLE IF EXISTS {self.wstable} CASCADE")
+                self.wstable = None
+            self.con.commit()
+        except psycopg2.Error as e:
+            logger.error("Unable to delete tables: %s", str(e))
+            raise Exception(e)
